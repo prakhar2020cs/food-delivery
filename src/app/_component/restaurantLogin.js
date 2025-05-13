0db5a9e63e6c0e4a098977239d643039e21db98f
Prakhar Sharma <Prakhar06035@gmail.com>
2025-05-13 10:47:24 +0530

    search working, Db connection issue due to .env fixed

--- src/app/_component/restaurantLogin.js
+++ src/app/_component/restaurantLogin.js
@@ -1,24 +1,23 @@
-"use client"
-import Cookies from 'js-cookie';
+"use client";
+import Cookies from "js-cookie";
 
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import style from "./restaurant.module.css";
 import Link from "next/link";
+import { toast, ToastContainer } from "react-toastify";
 
 const Login = () => {
-
   const [loading, setLoading] = useState(false);
 
-  const [userEmail, setEmail] = useState('');
-  const [userPassword, setPassword] = useState('');
+  const [userEmail, setEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
   // let [tokenToVerify, setTokenToVerify] = useState(null);
   const router = useRouter();
   // let tokenToVerify = null;
 
-
   useEffect(() => {
-    console.log("use effect")
+    console.log("use effect");
     // let storedUser = localStorage.getItem("registeredUser");
     // console.log("storedUser", storedUser);
     // const parsedtoken = JSON.parse(storedUser);
@@ -26,34 +25,29 @@ const Login = () => {
     // console.log("tokenToverify---login page", tokenToVerify);
 
     // if no token st
- 
+
     const verifyToken = async () => {
       let tokenToVerify = Cookies.get("token"); // Get the token from cookies
 
+      //      if (Cookies.get("token")) {
+      //   console.log("signup-token, redirected to dashboard")
+      //   router.replace("/dashboard")
+      // }
 
-    //      if (Cookies.get("token")) {
-    //   console.log("signup-token, redirected to dashboard")
-    //   router.replace("/dashboard")
-    // }
-
-
-      console.log("verifying token...")
+      console.log("verifying token...");
       try {
-
-
-
         if (!tokenToVerify) {
           console.log("NO token found");
 
           return;
         }
-        console.log("check2")
-      
+        console.log("check2");
+
         let tokenVerify = await fetch("/api/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${tokenToVerify}`,
+            Authorization: `Bearer ${tokenToVerify}`,
           },
           // body: JSON.stringify({ tokenToVerify: tokenToVerify })
         });
@@ -65,62 +59,55 @@ const Login = () => {
 
           router.push("/dashboard");
         }
-
       } catch (error) {
-
         console.log("error verifying token", error);
       }
-
-
     };
     verifyToken();
-
-
-
-  }, [])
+  }, []);
 
   const handleLogin = async (e) => {
-    setLoading(true)
+    setLoading(true);
 
     if (!userEmail || !userPassword) {
       window.alert("Email and password are required");
+      setLoading(false);
       return;
     }
 
-    e.preventDefault();
-    let result = await fetch('/api/login', {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userEmail, userPassword })
-
-    });
-    let response = await result.json();
-    if (response.success) {
-      const { userData, token } = response;
-      console.log(userData, token);
-      // localStorage.setItem("registeredUser", JSON.stringify({ userData, token }));
-      Cookies.set("token", token, {
-        expires: 7, // Cookie expires in 7 days
-
-        sameSite: 'strict' // Protect against CSRF
+    try {
+      let result = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userEmail, userPassword }),
       });
+      let response = await result.json();
+      if (response.success) {
+        const { userData, token } = response;
+        console.log(userData, token);
+        // localStorage.setItem("registeredUser", JSON.stringify({ userData, token }));
+        Cookies.set("token", token, {
+          expires: 7, // Cookie expires in 7 days
 
+          sameSite: "strict", // Protect against CSRF
+        });
 
-      router.push("/dashboard");
-
-
-
-    } else {
-      setLoading(false)
-      window.alert("invalid user");
+        router.push("/dashboard");
+      } else {
+        toast.error("invalid user");
+      }
+    } catch (e) {
+      console.log("some error occurred", e);
+      toast.error("some error occurred");
+    } finally {
+      setLoading(false);
     }
-    setLoading(false)
-
-  }
+  };
   // console.log(email);
 
   return (
     <div>
+      <ToastContainer />
       <h2>Login</h2>
       <form className={style.login}>
         <div className={style.imgContainer}>
@@ -128,23 +115,46 @@ const Login = () => {
         </div>
 
         <div className={style.container}>
-          <label htmlFor="uname" ><b>Email</b></label>
-          <input className={style.formInput} type="text" placeholder="Enter Username" value={userEmail} onChange={(e) => setEmail(e.target.value)} name="email" required />
+          <label htmlFor="uname">
+            <b>Email</b>
+          </label>
+          <input
+            className={style.formInput}
+            type="text"
+            placeholder="Enter Username"
+            value={userEmail}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            required
+          />
 
-          <label htmlFor="psw"><b>Password</b></label>
-          <input type="password" value={userPassword} onChange={(e) => { setPassword(e.target.value) }} placeholder="Enter Password" name="password" required />
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            value={userPassword}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
 
-          <button type="submit" onClick={handleLogin} disabled={loading}>{loading?"Logging In..":  "Login"}</button>
-        
+          <button type="submit" onClick={handleLogin} disabled={loading}>
+            {loading ? "Logging In.." : "Login"}
+          </button>
         </div>
 
         <div className={style.container}>
-
-          <span className="psw"> <Link href="/forgotpassword">Forgot password?</Link></span>
+          <span className="psw">
+            {" "}
+            <Link href="/forgotpassword">Forgot password?</Link>
+          </span>
         </div>
       </form>
-
     </div>
-  )
-}
+  );
+};
 export default Login;
--- src/app/lib/dbConnect.js
+++ src/app/lib/dbConnect.js
@@ -3,13 +3,14 @@ import { connectionStr } from "./db";
 
 export async function dbConnect() {
   try {
+
     if (mongoose.connection.readyState >= 1) {
       return;
     }
-    
+    console.log("Connecting to MongoDB with connection string:", connectionStr);
     return await mongoose.connect(connectionStr);
   } catch (error) {
-    console.log("MongoDB connection error", error);
+    console.log("MongoDB connection error", error.message);
     throw new Error("Failed to connect to database");
   }
 }
